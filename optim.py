3420c3e2e7c066a6e8ef80cc7c919420b54f7c9d
Zayar Khin <arandomname@gmail.com>
2019-05-07 12:58:22 -0400

    Update optim.py

--- optim.py
+++ optim.py
@@ -2,7 +2,6 @@
 We use an optimizer to adjust the parameters of our network based on the gradients computed during backpropagation
 """
 
-
 class Optimizer:
 	def step(self, net): #NeuralNet -> None
 		raise NotImplementedError
@@ -11,7 +10,6 @@ class SGD(Optimizer):
 	def __init__(self, lr=0.01): #float -> None
 		self.lr = lr
 
-		
 	def step(self, net): #NeuralNet -> None
 		for param, grad in net.params_and_grads():
 			param -= self.lr * grad
